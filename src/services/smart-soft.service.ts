85e849d4adb5005f23f9f4efc0ee57f5ee5fd44b
Isaac Williams <Isaaciyaye174@gmail.com>
2024-05-05 19:46:35 +0100

    new

--- src/services/smart-soft.service.ts
+++ src/services/smart-soft.service.ts
@@ -1,8 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { HttpService } from '@nestjs/axios';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import * as crypto from 'crypto';
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config'; // Import your SettingService
 import { AxiosRequestConfig } from 'axios';
 import {
@@ -91,7 +92,7 @@ export class SmartSoftService {
       if (!player)
         return {
           success: false,
-          message: 'Invalid Hash Signature',
+          message: 'Invalid Session ID',
         };
       game = await this.gameRepository.findOne({
         where: {
@@ -104,9 +105,11 @@ export class SmartSoftService {
         return await this.activateSession(resp.body['Token']);
         break;
       case 'GetBalance':
+        console.log('GetBalance');
         return await this.getBalance(resp.header['x-sessionid']);
         break;
       case 'Deposit':
+        console.log('Deposit');
         if (!game)
           return {
             success: false,
@@ -116,26 +119,94 @@ export class SmartSoftService {
         const placeBetPayload: PlaceCasinoBetRequest = {
           userId: player.userId,
           clientId: player.clientId,
-          roundId: resp.body.TransactionId,
+          roundId: resp.body.TransactionInfo.RoundId,
           transactionId: resp.body.TransactionId,
           gameId: game.gameId,
           stake: resp.body.Amount,
           winnings: 0,
         };
-        return await this.placeBet(placeBetPayload);
+        const bets: [] = await this.cacheManager.get('bets');
+        console.log('cache bets', bets);
+        if (!bets) {
+          const newBets = await this.cacheManager.set('bets', [
+            placeBetPayload,
+          ]);
+          console.log('cache bets 2', newBets);
+          return {
+            success: true,
+            message: 'Deposit',
+            data: {
+              Balance: resp.body.Amount,
+              TransactionId: resp.body.TransactionId,
+            },
+          };
+        }
+        const newBets = await this.cacheManager.set('bets', [
+          ...bets,
+          placeBetPayload,
+        ]);
+        console.log('cache bets 3', newBets);
+
+        return {
+          success: true,
+          message: 'Deposit',
+          data: {
+            Balance: resp.body.Amount,
+            TransactionId: resp.body.TransactionId,
+          },
+        };
+        // return await this.placeBet(placeBetPayload);
         break;
       case 'Withdraw':
         const settlePayload: CreditCasinoBetRequest = {
           transactionId: resp.body.TransactionId,
           winnings: resp.body.Amount,
         };
+        const bet_s: [] = await this.cacheManager.get('bets');
+        console.log('cache bets 2', bet_s);
+
+        if (!bet_s) {
+          return {
+            success: false,
+            message: 'bET not in system',
+          };
+        }
+        return {
+          success: true,
+          message: 'Deposit',
+          data: {
+            Balance: resp.body.Amount,
+            TransactionId: resp.body.TransactionId,
+          },
+        };
         return await this.settle(settlePayload);
         break;
-      case 'IssueClientGifts':
+      case 'RollbackTransaction':
         const reversePayload: RollbackCasinoBetRequest = {
           transactionId: resp.body.TransactionId,
         };
+        const bet__s: any[] = await this.cacheManager.get('bets');
+        console.log('cache bets 2', bet__s);
+
+        if (!bet__s || !bet__s.length) {
+          return {
+            success: false,
+            message: 'bET not in system',
+          };
+        }
+
+        return {
+          success: true,
+          message: 'Deposit',
+          data: {
+            Balance: resp.body.Amount,
+            TransactionId: resp.body.TransactionId,
+          },
+        };
         return await this.rollbackTransaction(reversePayload);
+        break;
+      default:
+        throw new NotFoundException('Unknown provider');
         break;
     }
   }
@@ -167,6 +238,7 @@ export class SmartSoftService {
 
   // Activate Player Session
   async activateSession(token) {
+    console.log('token', token);
     const player = await this.playerRepository.findOne({
       where: {
         authCode: token,
@@ -193,6 +265,7 @@ export class SmartSoftService {
 
   // Get Player Balance
   async getBalance(token) {
+    console.log('getBalance', token);
     const player = await this.playerRepository.findOne({
       where: {
         virtualToken: token,
@@ -200,9 +273,12 @@ export class SmartSoftService {
     });
     if (player) {
       //TODO: USE PLAYER UserID AND ClientID to get balance from wallet service;
-      const wallet = await this.walletService
-        .getWallet({ userId: player.userId, clientId: player.clientId });
-        
+      const wallet = await this.walletService.getWallet({
+        userId: player.userId,
+        clientId: player.clientId,
+      });
+
+      console.log('wallet', wallet);
       if (wallet.success) {
         return {
           success: true,
