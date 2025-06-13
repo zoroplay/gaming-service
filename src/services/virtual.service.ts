989f9bd02bcb9298b723486d755d69fdc654e05c
Isoboye Ibanibo <elsopi02@yahoo.com>
2025-06-13 05:43:09 +0100

    debugging xpress

--- src/services/virtual.service.ts
+++ src/services/virtual.service.ts
@@ -56,7 +56,7 @@ export class VirtualService {
         };
 
         const hashStr = `${data.playerId}${data.currency}${data.balance}${data.sessionId}${data.group}${data.timestamp}${data.requestId}${privateKeyQuery.value}`;
-
+        console.log(hashStr)
         data.fingerprint = MD5(hashStr).toString();
 
         return {
